0f68d5171d12ae8cb5882cfadc448271d69ad2d7
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-09-09 22:51:06 +0100

    got the basic model for a shoe down, 23 more to go 😫

--- client/src/models/shoes.js
+++ client/src/models/shoes.js
@@ -1,4 +1,29 @@
 var beigeHeeledBoot = {
   name: "Beige Heeled Boot",
-  image: 
+  image: "./css/image/beige-heeled-boot.jpg",
+  colour: "Beige",
+  flat: false,
+  bigHeel: false,
+  smallHeel: true,
+  boot: true,
+  wedge: false,
+  laces: false,
+  studded: false,
+  openToe: false,
+  openHeel: false
+}
+
+var beigeHeeledBoot = {
+  name: "Beige Heeled Boot",
+  image: "./css/image/beige-heeled-boot.jpg",
+  colour: "Beige",
+  flat: false,
+  bigHeel: false,
+  smallHeel: true,
+  boot: true,
+  wedge: false,
+  laces: false,
+  studded: false,
+  openToe: false,
+  openHeel: false
 }
