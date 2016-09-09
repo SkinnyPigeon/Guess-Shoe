4649290327ac3fe31de186b86be794859ef927e6
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-09-09 23:13:37 +0100

    about a third of the way through creating the shoe objects

--- client/src/models/shoes.js
+++ client/src/models/shoes.js
@@ -1,21 +1,22 @@
 var beigeHeeledBoot = {
-  name: "Beige Heeled Boot",
+  name: "Puffy Ugly Beige Shoe",
   image: "./css/image/beige-heeled-boot.jpg",
   colour: "Beige",
   flat: false,
-  bigHeel: false,
-  smallHeel: true,
+  bigHeel: true,
+  smallHeel: false,
   boot: true,
   wedge: false,
   laces: false,
   studded: false,
+  buckle: false,
   openToe: false,
   openHeel: false
 }
 
-var beigeHeeledBoot = {
-  name: "Beige Heeled Boot",
-  image: "./css/image/beige-heeled-boot.jpg",
+var beigeLowBootStudded = {
+  name: "Spikey Balsa-Wood Heeled Boot (Beige)",
+  image: "./css/image/beige-low-boot-studded",
   colour: "Beige",
   flat: false,
   bigHeel: false,
@@ -23,7 +24,139 @@ var beigeHeeledBoot = {
   boot: true,
   wedge: false,
   laces: false,
-  studded: false,
+  studded: true,
+  buckle: false,
   openToe: false,
   openHeel: false
 }
+
+var beigeOpenToeBuckleStuddedHeel = {
+  name: "Impractical Plus Beige Bleugh",
+  image: "./css/image/beige-open-toe-buckle-studded-heel.jpg",
+  colour: "Beige",
+  flat: false,
+  bigHeel: true,
+  smallHeel: false,
+  boot: false,
+  wedge: false,
+  laces: false,
+  studded: true,
+  buckle: true,
+  openToe: true,
+  openHeel: false
+}
+
+var beigeOpenToeOpenHeelWedge = {
+  name: "The Unlwalkable In",
+  image: "./css/image/beige-open-toe-open-heel-wedge.jpg",
+  colour: "Beige",
+  flat: false,
+  bigHeel: false,
+  smallHeel: false,
+  boot: false,
+  wedge: true,
+  laces: true,
+  studded: false,
+  buckle: false,
+  openToe: true,
+  openHeel: true
+}
+
+var beigeWedgeBoot = {
+  name: "Tiptoe Sweat Boxes - Beige",
+  image: "./css/image/beige-wedge-boot.jpg",
+  colour: "Beige",
+  flat: false,
+  bigHeel: false,
+  smallHeel: false,
+  boot: false,
+  wedge: true,
+  laces: false,
+  studded: false,
+  buckle: false,
+  openToe: false,
+  openHeel: false
+}
+
+var blackFlatBuckle = {
+  name: "A Normal Black Shoe",
+  image: "./css/image/black-flat-buckle.jpg",
+  colour: "Black",
+  flat: true,
+  bigHeel: false,
+  smallHeel: false,
+  boot: false,
+  wedge: false,
+  laces: false,
+  studded: false,
+  buckle: true,
+  openToe: false,
+  openHeel: false
+}
+
+var blackHeelBootOpenToe = {
+  name: "Walk Awkard In Black",
+  image: "./css/image/black-heeled-boot-open-toe.jpg",
+  colour: "Black",
+  flat: false,
+  bigHeel: true,
+  smallHeel: false,
+  boot: true,
+  wedge: false,
+  laces: false,
+  studded: false,
+  buckle: false,
+  openToe: true,
+  openHeel: false
+}
+
+var blackHeeledLaces = {
+  name: "Impossible Heel",
+  image: "./css/image/black-heeled-laces.jpg",
+  colour: "Black",
+  flat: false,
+  bigHeel: true,
+  smallHeel: false,
+  boot: false,
+  wedge: false,
+  laces: true,
+  studded: false,
+  buckle: false,
+  openToe: false,
+  openHeel: false
+}
+
+var blackHeeledToe = {
+  name: "Also Available With Presnapped Heel",
+  image: "./css/image/black-heeled-toe.jpg",
+  colour: "Black",
+  flat: false,
+  bigHeel: true,
+  smallHeel: false,
+  boot: false,
+  wedge: false,
+  laces: false,
+  studded: false,
+  buckle: false,
+  openToe: true,
+  openHeel: false
+}
+
+
+
+
+flat: false,
+bigHeel: false,
+smallHeel: false,
+boot: false,
+wedge: false,
+laces: false,
+studded: false,
+buckle: false,
+openToe: false,
+openHeel: false
+
+
+
+
+
