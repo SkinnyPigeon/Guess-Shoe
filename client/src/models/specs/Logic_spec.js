d5f9280df656271de6f5b1a234cb54f1688bad39
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-09-10 10:07:15 +0100

    test is speaking to the logic file and shoe can be passed in

--- client/src/models/specs/Logic_spec.js
+++ client/src/models/specs/Logic_spec.js
@@ -1,1 +1,12 @@
-var Logic = require( './Logic' );
+var Logic = require( '../Logic' );
+var shoes = require( '../shoes' );
+var assert = require( 'chai' ).assert;
+
+describe( 'The Logic: ', function() {
+
+  it( 'Should know the name of the shoe', function() {
+    var logic = new Logic( shoes[0] );
+    assert.equal( "Puffy Ugly Beige Shoe", logic.shoe.name );
+  });
+
+});
