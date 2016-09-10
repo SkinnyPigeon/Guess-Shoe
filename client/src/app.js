158abebdd02af83808f47d6bb7497d4fa7fcd960
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-09-10 11:46:13 +0100

    started the app, will try to get the pictures on screen

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,16 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var playerShoes = require( './models/playerShoes' );
+var opponentShoes = require( './models/opponentShoes' );
+var Game = require( './models/Game' );
+var WhoBox = require( './components/WhoBox' );
 
 window.onload = function(){
+
+  var game = new Game( playerShoes, opponentShoes );
+
   ReactDOM.render(
-    <h1> App Started </h1>,
+    <WhoBox game={ game }/>,
     document.getElementById('app')
   );
 }
---
+++ client/src/components/WhoBox.jsx
@@ -0,0 +1,13 @@
+var React = require( 'react' );
+
+var WhoBox = React.createClass({
+
+  render: function() {
+    return(
+      <h1>App Is Working</h1>
+    )
+  }
+
+})
+
+module.exports = WhoBox;
